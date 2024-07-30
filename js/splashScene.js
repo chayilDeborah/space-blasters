7883428f51791fb0349792beb881bb3e78e3fc8f
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-07-30 17:02:03 +0100

    feat: create splashScrene file

---
+++ js/splashScene.js
@@ -0,0 +1,30 @@
+/* global Phaser */
+
+
+// This is the Splash Scene
+
+class SplashScene extends Phaser.Scene {
+    constructor () {
+      super({ key: 'splashScene' })
+  
+
+    }
+  
+    init (data) {
+      this.cameras.main.setBackgroundColor('#ffffff')
+    }
+  
+    preload () {
+      console.log('Splash Scene')
+    }
+  
+    create (data) {
+         }
+  
+    update (time, delta) {
+    
+
+    }
+  }
+  
+  export default SplashScene
