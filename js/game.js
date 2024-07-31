f33f0af0870eba4122f5463870c0b81a5178c604
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-07-31 15:36:52 +0100

    feat: add title page background image and game title

--- js/game.js
+++ js/game.js
@@ -4,13 +4,13 @@
 
 import SplashScene from './splashScene.js'
 import TitleScene from './titleScene.js'
-// import MenuScene from './menuScene.js'
+import MenuScene from './menuScene.js'
 // import GameScene from './gameScene.js'
 
 // Our game scenes
 const splashScene = new SplashScene()
 const titleScene = new TitleScene()
-// const menuScene = new MenuScene()
+const menuScene = new MenuScene()
 // const gameScene = new GameScene()
 
 //* Game scene */
@@ -39,7 +39,7 @@ const game = new Phaser.Game(config)
 // NOTE: remember any "key" is global and CAN NOT be reused!
 game.scene.add('splashScene', splashScene)
 game.scene.add('titleScene', titleScene)
-// game.scene.add('menuScene', menuScene)
+game.scene.add('menuScene', menuScene)
 // game.scene.add('gameScene', gameScene)
 
 // start title
--- js/titleScene.js
+++ js/titleScene.js
@@ -1,18 +1,15 @@
 /* global Phaser */
 
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+
 // This is the Title Scene
 
 class TitleScene extends Phaser.Scene {
   constructor () {
     super({ key: 'titleScene' })
 
-    // this.titleSceneBackgroundImage = null
+    this.titleSceneBackgroundImage = null
     // this.titleSceneText = null
-    // this.titleSceneTextStyle = { font: '200px Times', fill: '#fde4b9', align: 'center' }
+    this.titleSceneTextStyle = { font: '200px Times', fill: '#fde4b9', align: 'center' }
   }
 
   init (data) {
@@ -21,15 +18,15 @@ class TitleScene extends Phaser.Scene {
 
   preload () {
     console.log('Title Scene')
-    // this.load.image('titleSceneBackground', 'assets/aliens_screen_image.jpg')
+    this.load.image('titleSceneBackground', './assets/assets/aliens_screen_image.jpg')
   }
 
   create (data) {
-    // this.titleSceneBackgroundImage = this.add.sprite(0, 0, 'titleSceneBackground').setScale(2.75)
-    // this.titleSceneBackgroundImage.x = 1920 / 2
-    // this.titleSceneBackgroundImage.y = 1080 / 2
+    this.titleSceneBackgroundImage = this.add.sprite(0, 0, 'titleSceneBackground').setScale(2.75)
+    this.titleSceneBackgroundImage.x = 1920 / 2
+    this.titleSceneBackgroundImage.y = 1080 / 2
 
-    // this.titleSceneText = this.add.text(1920 / 2, (1080 / 2) + 350, 'Space Aliens', this.titleSceneTextStyle).setOrigin(0.5)
+    this.titleSceneText = this.add.text(1920 / 2, (1080 / 2) + 350, 'Space Blasters', this.titleSceneTextStyle).setOrigin(0.5)
   }
 
   update (time, delta) {
