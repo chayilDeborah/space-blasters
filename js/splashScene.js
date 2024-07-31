50dd02daf506bf423e80e6748f767770c7a7dfaa
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-07-31 15:12:06 +0100

    feat: add background image to game

--- js/splashScene.js
+++ js/splashScene.js
@@ -16,15 +16,20 @@ class SplashScene extends Phaser.Scene {
   
     preload () {
       console.log('Splash Scene')
+      this.load.image('splashSceneBackground', './assets/assets/splashSceneImage.png')
     }
   
     create (data) {
+      this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
+      this.splashSceneBackgroundImage.x = 1920 / 2
+      this.splashSceneBackgroundImage.y = 1080 / 2
          }
   
-    update (time, delta) {
-    
-
-    }
+         update (time, delta) {
+          if (time > 3000) {
+            this.scene.switch('titleScene')
+          }
+        }
   }
   
   export default SplashScene
