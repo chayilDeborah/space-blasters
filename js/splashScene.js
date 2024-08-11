721565dc1edac81d4f4f5d1799d7f1860a2d65cc
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-08-12 00:31:32 +0100

    feat: complete game

--- js/splashScene.js
+++ js/splashScene.js
@@ -1,35 +1,38 @@
 /* global Phaser */
 
-
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
 // This is the Splash Scene
 
 class SplashScene extends Phaser.Scene {
-    constructor () {
-      super({ key: 'splashScene' })
-  
+  constructor () {
+    super({ key: 'splashScene' })
 
-    }
-  
-    init (data) {
-      this.cameras.main.setBackgroundColor('#ffffff')
-    }
-  
-    preload () {
-      console.log('Splash Scene')
-      this.load.image('splashSceneBackground', './assets/assets/splashSceneImage.png')
-    }
-  
-    create (data) {
-      this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
-      this.splashSceneBackgroundImage.x = 1920 / 2
-      this.splashSceneBackgroundImage.y = 1080 / 2
-         }
-  
-         update (time, delta) {
-          if (time > 3000) {
-            this.scene.switch('titleScene')
-          }
-        }
+    this.splashSceneBackgroundImage = null
   }
-  
-  export default SplashScene
+
+  init (data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
+
+  preload () {
+    console.log('Splash Scene')
+    this.load.image('splashSceneBackground', './assets/splashSceneImage.png')
+  }
+
+  create (data) {
+    this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
+  }
+
+  update (time, delta) {
+    if (time > 3000) {
+      this.scene.switch('titleScene')
+    }
+  }
+}
+
+export default SplashScene
--- js/titleScene.js
+++ js/titleScene.js
@@ -1,6 +1,9 @@
 /* global Phaser */
 
-
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
 // This is the Title Scene
 
 class TitleScene extends Phaser.Scene {
@@ -8,7 +11,7 @@ class TitleScene extends Phaser.Scene {
     super({ key: 'titleScene' })
 
     this.titleSceneBackgroundImage = null
-    // this.titleSceneText = null
+    this.titleSceneText = null
     this.titleSceneTextStyle = { font: '200px Times', fill: '#fde4b9', align: 'center' }
   }
 
@@ -18,7 +21,7 @@ class TitleScene extends Phaser.Scene {
 
   preload () {
     console.log('Title Scene')
-    this.load.image('titleSceneBackground', './assets/assets/aliens_screen_image.jpg')
+    this.load.image('titleSceneBackground', 'assets/aliens_screen_image.jpg')
   }
 
   create (data) {
@@ -26,7 +29,7 @@ class TitleScene extends Phaser.Scene {
     this.titleSceneBackgroundImage.x = 1920 / 2
     this.titleSceneBackgroundImage.y = 1080 / 2
 
-    this.titleSceneText = this.add.text(1920 / 2, (1080 / 2) + 350, 'Space Blasters', this.titleSceneTextStyle).setOrigin(0.5)
+    this.titleSceneText = this.add.text(1920 / 2, (1080 / 2) + 350, 'Space Aliens', this.titleSceneTextStyle).setOrigin(0.5)
   }
 
   update (time, delta) {
