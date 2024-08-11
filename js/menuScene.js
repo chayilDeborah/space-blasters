5aa23356d8d19e12a152233eaa08a9b34b1868bf
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-08-12 00:33:20 +0100

    fix

--- js/menuScene.js
+++ js/menuScene.js
@@ -1,9 +1,5 @@
 /* global Phaser */
 
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
 // This is the Menu Scene
 
 class MenuScene extends Phaser.Scene {
--- js/titleScene.js
+++ js/titleScene.js
@@ -1,8 +1,5 @@
 /* global Phaser */
 
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
 // Created on: Sep 2020
 // This is the Title Scene
 
